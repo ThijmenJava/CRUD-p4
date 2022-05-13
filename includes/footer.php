1f1079ea094baa654cdd38ab93326f24e7ddb37e
ThijmenJava <1194425@student.roc-nijmegen.nl>
2022-05-13 10:02:03 +0200

    merge main

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,10 @@
-<section class="">
-    <div>
-
+<section class="sectionfooter">
+    <div class="footer">
+        <div class="copyright">
+            <p>TTT Time To Travel</p>
+        </div>
+        <div class="socialmedia">
+            <p>sadbvastucdyas</p>
+        </div>      
     </div>    
 </section>

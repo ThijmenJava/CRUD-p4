5d51dab77dd8d7e24a3bb07fe176fb3163eee245
ThijmenJava <1194425@student.roc-nijmegen.nl>
2022-05-13 09:30:46 +0200

    merge main

--- index.php
+++ index.php
@@ -6,11 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Home</title>
 </head>
     <body>
-        <?php include("includes/header/header.php") ?>
-        <div class="slideShow" id="slideshow"></div>
+        <?php include("includes/header.php") ?>
+
+        <section class="main_section">
+            <main class="main_content">
+                <div class="slideShow" id="slideshow">
+                    <div class="red">
+
+                    </div>
+                    <div class="zoekblak">
+
+                    </div>
+                    <div class=""blauw>
+
+                    </div>
+                </div>                
+            </main>
+        </section>
+
     </body>
 <script src="js/home.js"></script>
 </html>

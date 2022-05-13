e5d2e7eada21bcbe42a75ce923f10c1dfce2d7cf
Thijmen <94443778+ThijmenJava@users.noreply.github.com>
2022-05-13 10:05:36 +0200

    Footer (#5)
    * test
    
    * merge main
    
    * merge main
    
    * merge main

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<section class="sectionfooter">
+    <div class="footer">
+        <div class="copyright">
+            <p>TTT Time To Travel</p>
+        </div>
+        <div class="socialmedia">
+            <p>sadbvastucdyas</p>
+        </div>      
+    </div>    
+</section>
---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <nav>
+        <div class="header-logo"></div>
+        <ul class="header-ul">
+            <li><a href="index.php">Home</a></li>
+            <li><a href=".php">Boeken</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Inloggen</a></li>
+        </ul>
+    </nav>
+</header>

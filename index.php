71af98c1aefcb0ce27f1ec90c3daf214dbc8ea6b
ThijmenJava <1194425@student.roc-nijmegen.nl>
2022-05-18 15:13:46 +0200

    merge master

--- index.php
+++ index.php
@@ -17,49 +17,68 @@
     <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E "/>
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Home</title>
 </head>
-<?php include("includes/header.php") ?>
-
-        <section class="main_section">
-            <main class="main_content">
-                <div class="slideShow" id="slideshow">
-                    <div class="zoekblak">
-                        <div class="zoek-titel">
-                            <h2>Zoek vlucht</h2>
-                        </div>
-                        <form class="plek-form" action="" method="post">
-                            <div class="container-labels">
-                                <div class="labels-plek">
-                                    <h3>Vanaf</h3>
-                                </div>
-                                <div class="labels-plek">
-                                    <h3>Naar</h3>
-                                </div>
-                            </div>
-                            <div class="container-input">
-                                <div class="input-plek">
-                                    <input class="input" type="text">
-                                </div>
-                                <div class="input-plek">
-                                    <input type="text">
-                                </div>
-                            </div>
-                            <div>
-
-                            </div>
-                            <div>
-
-                            </div>
-                            <div>
-
-                            </div>
-                        </form>
+<body>
+    
+    <?php include("includes/header.php") ?>
+    
+    <section class="main_section">
+        <main class="main_content">
+            <div class="slideShow" id="slideshow">
+                <div class="zoekblak">
+                    <div class="zoek-titel">
+                        <h2>Zoek vlucht</h2>
                     </div>
-                </div>                
-            </main>
-        </section>
+                    <form class="plek-form" action="" method="post">
+                        <div class="container-labels">
+                            <div class="labels-plek">
+                                <h3>Vanaf</h3>
+                            </div>
+                            <div class="labels-plek">
+                                <h3>Naar</h3>
+                            </div>
+                        </div>
+                        <div class="container-input">
+                            <div class="input-plek">
+                                <div class="black img-border-right">
+                                    <img class="vliegtuig-img" src="img/airplane-png.png" alt="">
+                                </div>
+                                <input class="input border-right" type="text">
+                            </div>
+                            <div class="input-plek">
+                                <input class="input border-left" type="text">
+                                <div class="black img-border-left border-left rotate">
+                                    <img class="vliegtuig-img" src="img/airplane-png.png" alt="">                                    
+                                </div>
+                            </div>
+                            <div class="margin input-plek">
+                                <div class="black border-left">
+                                    <i class="fa-solid fa-calendar-days calander-size"></i>
+                                </div>
+                                <input type="date" min="<?php echo date("Y-m-d"); ?>" class="input border-right">
+                            </div>
+                        </div>
+
+                        </div>
+                        <div>
+                            <div>
+
+                            </div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>                
+        </main>
+    </section>
+</body>
 
 <script src="js/home.js"></script>
+<script src="https://kit.fontawesome.com/385f3b2853.js" crossorigin="anonymous"></script>
 
 <?php include("includes/footer.php"); ?>
+
+</html>

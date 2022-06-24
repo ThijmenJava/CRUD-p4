e7328e6042b45b4451ffc56253302db032bc4cbd
ThijsHeerze <94444145+ThijsHeerze@users.noreply.github.com>
2022-06-24 08:39:09 +0200

    Thijs (#22)
    * thijs
    
    * thijs

--- overons.php
+++ overons.php
@@ -11,28 +11,39 @@
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&family=Titillium+Web:ital,wght@1,300&display=swap" rel="stylesheet">
     <title>Over ons</title>
 </head>
-<body class="over-ons-body">
-    <?php include_once('includes/header.php'); ?>
-        <div class="over-ons">
-            <div class="informatie">
-                <h2 class="informatie-titel">Informatie over ons</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis accusamus eligendi similique assumenda, velit modi ducimus nihil fugit, iure quasi magnam mollitia impedit at esse consectetur, libero nemo corporis aut?</p>
-            </div>
-            <div class="informatie-over-ons">
-                <div class="tt">
+    <body class="over-ons-body">
+        <?php include_once('includes/header.php'); ?>
+            <div class="over-ons">
+                <div class="informatie">
                     <div class="container">
-                        <div class="naam">
-                            <h3 class="naam-titel">Thijmen Lemmens</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod animi corporis voluptatum aliquam, corrupti voluptatibus excepturi id, magnam nemo nostrum minima est. Ipsum explicabo ullam veritatis suscipit vero soluta. Repellendus!</p>
-                        </div>
-                        <div class="naam">
-                            <h3 class="naam-titel">Thijs Heerze</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aliquam illo natus, optio dignissimos explicabo itaque ratione deserunt molestias sequi laudantium corporis sapiente ea placeat numquam iste eos nesciunt dolor?</p>
-                        </div>
+                        <h2 class="informatie-titel">Over</h2><h2 class="blue-titel">Time To Travel</h2>
+                    </div>
+                    <div class="container">
+                        <a href="#naar-info"><button class="naar-info">Naar informatie</button></a>
                     </div>
                 </div>
+                <div class="informatie-over-ons">
+                    <a id="naar-info" href="naar-info"></a>
+                    <h2 class="titel-werknemers">Werknemers</h2>
+                        <div class="container">
+                            <div class="naam">
+                                <h3 class="naam-titel">Thijmen Lemmens</h3>
+                                <p class="naam-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolorem aspernatur repudiandae sequi?</p>
+                            </div>
+                            <div class="naam">
+                                <h3 class="naam-titel">Thijs Heerze</h3>
+                                <p class="naam-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel saepe a iusto dolor!</p>
+                            </div>
+                        </div>
+                            <div class="container-contact">
+                                <div class="neem-contact-op">
+                                    <h3 class="contact-titel">Wil je contact met ons opnemen?</h3>
+                                    <p>Neem contact met ons op hieronder</p>
+                                    <button class="contact-button">Contact</button>
+                                </div>
+                        </div>
+                    </div>
             </div>
-        </div>
-    <?php include_once('includes/footer.php'); ?>
+        <?php include_once('includes/footer.php'); ?>
     </body>
 </html>
